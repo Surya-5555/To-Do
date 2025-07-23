9a9e9cd7585594eb77a68299ab4e60a18ba945e9
surya <suryaravichandran5555@gmail.com>
2025-07-24 00:52:28 +0530

    Completed setting up nodemon

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,30 @@ import express from "express"; // => Change type to module in package.json
 
 const app = express();
 
+// Route
 app.get("/api/notes" , (req , res) =>{
     // Send the notes
-    res.statusCode(200).end("You got 5 notes");
+    res.status(200).send("You got 20 notes");
 });
+// Route
 
 app.post("/api/notes" , (req , res) =>{
-    // Send the notes
-    res.statusCode(201).end("Your notes have been created successfully");//201 => New resource created
+    // Create a note
+    res.status(201).json({ message: "Note created successfully" });//201 => New resource created
 });
-    
+
+app.put("/api/notes/:id" , (req , res) =>{ // id to go into database
+    // Create a note
+    res.status(200).json({ message: "Note updated successfully" });//201 => New resource created
+});    
+
+app.delete("/api/notes/:id" , (req , res) =>{ // id to go into database
+    // Create a note
+    res.status(200).json({ message: "Note deleted successfully" });//201 => New resource created
+});   
+
+// https:/localhost:5001/api/notes/555 - Endpoint for the routes
+
 app.listen(5001 , () => {
     console.log("Server is running on port 5001");
 })
-
