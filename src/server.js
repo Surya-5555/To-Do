8042967d6bc70fc8c656b120b1ccc709bdcd02ca
surya <suryaravichandran5555@gmail.com>
2025-07-28 15:27:32 +0530

    test

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express"; // => Change type to module in package.json
+// const express = require("express"); => Change type to commonjs in package.json 
+import notesRoutes from "./routes/notesRoutes.js"; // Import the notes routes
+import {connectDB} from "./config/db.js"; // MONGO DB Connection
+
+const app = express();
+
+connectDB(); // Connect to MongoDB
+
+app.use("/api/notes" , notesRoutes); // Use the notes routes
+
+
+app.listen(5001 , () => {
+    console.log("Server is running on port 5001");
+})
+
+
+
+
