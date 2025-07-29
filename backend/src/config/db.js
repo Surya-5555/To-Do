4afa19207f6c22f222b474d0b1adc259bba65f39
Surya P R <suryaravichandran5555@gmail.com>
2025-07-29 12:39:16 +0530

    Added DB name in the connection url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect("mongodb+srv://suryaravichandran5555:e5Qyn1kFGHX4jPNv@cluster0.s8zodnu.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0cls");
+        await mongoose.connect("mongodb+srv://suryaravichandran5555:e5Qyn1kFGHX4jPNv@cluster0.s8zodnu.mongodb.net/Notes_db?retryWrites=true&w=majority&appName=Cluster0cls");
         console.log("MongoDB connected");
     }catch(error){
         console.error("MongoDB connection failed:", error);
