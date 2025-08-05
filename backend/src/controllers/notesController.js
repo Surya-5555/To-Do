243d52b9b63ffb0166883ec49b4d5d07fb2fa87a
Surya P R <suryaravichandran5555@gmail.com>
2025-08-05 23:47:37 +0530

    Noted time

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,3 +1,8 @@
+import Note from "../models/Note.js"; 
+
+
+
+
 // export const getAllNotes = (req , res) =>{
 //     // Send the notes
 //     res.status(200).send("You just fetched the notes");
@@ -5,25 +10,84 @@
 
 // Below is same with normal function
 
-export function getAllNotes(req , res){
+// export function getAllNotes(req , res){
+//     // Send the notes
+//     res.status(200).send("You just fetched the notes");
+// };
+export async function getAllNotes(req , res){
     // Send the notes
-    res.status(200).send("You just fetched the notes");
+    try {
+        const notes = await Note.find().sort({createdAt : -1}); // get all notes and to get seperate use --> {} inside find
+        // sort by createdAt which displays newest first
+        res.status(200).json(notes); 
+    } catch (error) {
+        console.error("Error in getAllNotes controller" , error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 
-export function createNote(req , res) {
+export async function getNoteById(req , res) {
+    // Get a note by ID
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({ message: "Note not found" });
+        }
+        res.status(200).json(note);
+    } catch (error) {
+        console.error("Error in getNoteById controller" , error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+
+export async function createNote(req , res) {
     // Create a note
-    res.status(201).json({ message: "Note created successfully" });//201 => New resource created
+    try {
+        const {title , content} = req.body;
+        console.log(title , content); // default it is undefined so modify in server.js(middleware)
+
+        const note = new Note({title , content})
+        const savedNote = await note.save();
+        res.status(201).json(savedNote);
+    } catch (error) {
+        console.error("Error in createNote controller" , error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
-export function updateNote(req , res) {
+
+
+
+export async function updateNote(req , res) {
     // Update a note
-    res.status(200).json({ message: "Note updated successfully" });//201 => New resource created
+    try {
+        const {title , content} = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title, content},{new:true});
+        if (!updatedNote) {
+            return res.status(404).json({ message: "Note not found" });
+        }
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        console.error("Error in update note controller" , error);
+        res.status(500).json({message:"internal server error"});
+    }
 };
 
 
-export function deleteNote(req , res) {
-    // Delete a note
-    res.status(200).json({ message: "Note deleted successfully" });//201 => New resource created
+
+
+export async function deleteNote(req , res) {
+    try {
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+        if (!deletedNote) {
+            return res.status(404).json({ message: "Note not found" });
+        }
+        res.status(200).json({ message: "Note deleted successfully" });
+    } catch (error) {
+        console.error("Error in delete note controller" , error);
+        res.status(500).json({message:"internal server error"});
+    }
 };
 
---
+++ backend/src/middleware/reteLimiter.js
@@ -0,0 +1,19 @@
+import ratelimit from "../config/upstash.js"; 
+
+
+const rateLimiter = async (req, res, next) => { // called in server.js // app.use(rateLimiter);
+    try {
+        const {success} = await ratelimit.limit("my-limit-key") //1.41.45 
+        if(!success){
+            return res.status(429).json({
+                message : "Too many requests, please try again later."
+            })
+        }
+        next();
+    } catch (error) {
+        console.log("Rate Limiter Error:", error);
+        next(error);
+    }
+}
+
+export default rateLimiter;
