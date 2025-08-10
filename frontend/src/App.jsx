507463095d23a164addcd401d1ab688139c571d5
Surya P R <suryaravichandran5555@gmail.com>
2025-08-11 01:56:59 +0530

    Setted up post CSS - Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import {Route , Routes} from "react-router";
 import HomePage from './pages/HomePage.jsx'; 
 import NoteDetailPage from './pages/NoteDetailPage.jsx';
 import CreatePage from './pages/CreatePage.jsx';
+import { toast } from 'react-hot-toast';
 const App = () => {
   return (
     <div>
+      <button onClick = {() => toast.success("Congrats")} className = "text-red-500 p-4bg">Click Me</button>
       <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreatePage />} />
@@ -15,3 +17,4 @@ const App = () => {
 };
 
 export default App;
+// rafc
