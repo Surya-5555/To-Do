31c3d23c9e28168350951af59d04d3bd1b802a85
Surya P R <suryaravichandran5555@gmail.com>
2025-08-31 23:12:19 +0530

    Revised and written the setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,8 @@ import { toast } from 'react-hot-toast';
 const App = () => {
   return (
     <div>
-      <button onClick = {() => toast.success("Congrats")} className = "text-red-500 p-4bg">Click Me</button>
+      <button onClick = {() => toast.success("Congrats")} className = "text-red-500 p-4 bg-pink-300">Click Me</button> 
+      {/* Stopped Here When I was Adding bg pink 300   */}
       <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreatePage />} />
