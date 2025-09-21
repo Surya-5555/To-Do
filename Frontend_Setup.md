8821862827157811caa106715ea73f42d46d8861
Surya P R <suryaravichandran5555@gmail.com>
2025-09-22 05:06:14 +0530

    Added daisyui to reuse the components

--- Frontend_Setup.md
+++ Frontend_Setup.md
@@ -6,3 +6,9 @@ npm run dev
 npm i react-router --> To add different pages
 npm i react-hot-toast --> Notifications
 rafce --> for boiler plate
+<Toaster /> --> put in main.jsx
+npm i daisyui@4.12.24 -D  --> For components
+
+daisyui:{
+    themes:["forest"],
+  }  --> Put it in tailwind config
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,18 @@ import CreatePage from './pages/CreatePage.jsx';
 import { toast } from 'react-hot-toast';
 const App = () => {
   return (
-    <div>
-      <button onClick = {() => toast.success("Congrats")} className = "text-red-500 p-4 bg-pink-300">Click Me</button> 
-      {/* Stopped Here When I was Adding bg pink 300   */}
+    <div data-theme="forest">
+      {/* <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button> */}
       <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-      <Route path="/note/:id" element={<NoteDetailPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
   )
